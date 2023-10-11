249ddb739200c483f7a7ebe1b7f2b7ecd2595cc0
Andrewsjjz <andresjimenez884@gmail.com>
2023-10-11 15:02:34 -0400

    cambios en cros

--- index.js
+++ index.js
@@ -16,23 +16,23 @@ conectarDB();
 // Configurar CORS
 const whitelist = [process.env.FRONTEND_URL];
 
- const corsOptions = {
-   origin: function (origin, callback) {
-     if (whitelist.includes(origin)) {
-       // Puede consultar la API
-       callback(null, true);
-     } else {
-//       // No esta permitido
-       callback(new Error("Error de Cors"));
-     }
-  },
- };
+//  const corsOptions = {
+//    origin: function (origin, callback) {
+//      if (whitelist.includes(origin)) {
+//        // Puede consultar la API
+//        callback(null, true);
+//      } else {
+// //       // No esta permitido
+//        callback(new Error("Error de Cors"));
+//      }
+//   },
+//  };
 
-const corsOptions = {
-  origin: 'https://steady-brigadeiros-0f80ba.netlify.app', // Sustituye con el dominio permitido
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Habilita el intercambio de cookies y encabezados de autenticación
-};
+// const corsOptions = {
+//   origin: 'https://steady-brigadeiros-0f80ba.netlify.app', // Sustituye con el dominio permitido
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true, // Habilita el intercambio de cookies y encabezados de autenticación
+// };
 
 // app.use(cors(corsOptions));
 
